d4daf11578ab58195c2aa88b8b07b6a81302e3c9
seabert yuan <seabert.s.yuan23z@gmail.com>
2020-12-08 15:42:59 -0700

    C# twenty2 WIP

---
+++ Twenty2 V1.2/MainWindow.xaml.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Timers;
+
+namespace Twenty2_V1._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        playMusic playMusic = new playMusic();
+
+        private void Openfile_click(object sender, RoutedEventArgs e)
+        {
+            string strfile = playMusic.fileFind();
+
+            Filedisplay.Text = $"Currently playing {strfile}";
+        }
+
+        #region Second Button Clicked
+
+        private void SecondButton_click(object sender, EventArgs e)
+        {
+
+            if (playMusic.BreakButtonState == 0)
+            {
+                SecondButtonTypeSwitch.Content = "Minutes";
+                playMusic.BreakButtonState = 1;
+                playMusic.breakButtonSwitchMinutes();
+            }
+
+            else
+            {
+
+                if (playMusic.BreakButtonState == 2)
+                {
+                    SecondButtonTypeSwitch.Content = "Seconds";
+                    playMusic.BreakButtonState = 0;
+                    playMusic.breakButtonSwitchSeconds();
+                }
+                else
+                {
+
+
+                    if (playMusic.BreakButtonState == 1)
+                    {
+                        SecondButtonTypeSwitch.Content = "Hours";
+                        playMusic.BreakButtonState = 2;
+                        playMusic.breakButtonSwitchHours();
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region First Button Clicked
+
+        private void Button_click(object sender, EventArgs e)
+        {
+
+            if (playMusic.ButtonState == 1)
+            {
+                ButtonTypeSwitch.Content = "Hours";
+                playMusic.ButtonState = 2;
+                playMusic.buttonSwitchHours();
+            }
+
+            else
+            {
+
+                if (playMusic.ButtonState == 2)
+                {
+                    ButtonTypeSwitch.Content = "Seconds";
+                    playMusic.ButtonState = 0;
+                    playMusic.buttonSwitchSeconds();
+                }
+                else
+                {
+                    if (playMusic.ButtonState == 0)
+                    {
+                        ButtonTypeSwitch.Content = "Minutes";
+                        playMusic.ButtonState = 1;
+                        playMusic.buttonSwitchMinutes();
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+
+        #region On/Off Button
+
+        private void On_click(object sender, EventArgs e)
+        {
+            if (playMusic.on == true)
+            {
+                this.OnButton.Content = "On";
+                playMusic.on = false;
+                playMusic.onTimer();
+
+            }
+            else
+            {
+                this.OnButton.Content = "Off";
+                playMusic.on = true;
+                playMusic.onTimer();
+            }
+        }
+
+        #endregion
+
+        private void ApplyButton_click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(AllowedTime.Text))
+            {
+                return;
+            }
+            else if(string.IsNullOrEmpty(AllowedBreakTime.Text))
+            {
+                return;
+            }
+            else
+            {
+                /*
+                playMusic.TimeAllowed = int.Parse(AllowedTime.Text);
+                playMusic.MultiplyFactor = int.Parse(AllowedBreakTime.Text);
+                playMusic.setTimer(playMusic.TimeAllowed, playMusic.MultiplyFactor);
+                */
+                playMusic.setTimer(int.Parse(AllowedTime.Text));
+            }
+            /*
+            if (string.IsNullOrEmpty(AllowedBreakTime.Text))
+            {
+                return;
+            }
+            else
+            {
+
+                playMusic.setTimer();
+            }
+            */
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            #region Defaults
+
+            playMusic playMusic = new playMusic();
+
+            //playMusic.BreakMultiplyFactor = 1000;
+            //playMusic.MultiplyFactor = 60000;
+            playMusic.breakButtonSwitchSeconds();
+            playMusic.buttonSwitchSeconds();
+
+            playMusic.ButtonState = 1;
+            playMusic.BreakButtonState = 0;
+
+            playMusic.TimeAllowed = 20;
+            playMusic.BreakTime = 20;
+
+            playMusic.file = new Uri(@"C:\Users\seabe\Music\Tobu - Candyland.mp3");
+
+            playMusic.on = true;
+
+            #endregion
+
+            playMusic.setTimer(playMusic.TimeAllowed);
+
+            #region Un-Minimize
+
+            myIcon = new NotifyIcon();
+            //myIcon.Icon = new System.Drawing.Icon(@"C:\Seabert's Visual Studio\TwentyTwo v1.1\Icon test.ico");
+
+            myIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(doubleclick);
+
+            #endregion
+
+        }
+
+        #region Minimize
+
+        private NotifyIcon myIcon;
+
+        void doubleclick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+        }
+
+        private void statechange(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                myIcon.Visible = true;
+                this.ShowInTaskbar = false;
+
+                myIcon.BalloonTipTitle = "TwentyTwo Minimized";
+                myIcon.BalloonTipText = "TwentyTwo is now minimized, click the icon to maximize.";
+                myIcon.ShowBalloonTip(500);
+
+            }
+
+            else if (this.WindowState == WindowState.Normal)
+            {
+                myIcon.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        #endregion
+
+        #region Number Filter
+
+        //Forces the text box to only allow numbers
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        #endregion
+
+        private void checkElapsed()
+        {
+            while (true)
+            {
+                if (playMusic.elapsedBool == true)
+                {
+                    playMusic.media.Open(playMusic.file);
+                }
+            }
+        }
+
+    }
+}
---
+++ Twenty2 V1.2/User Settings.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Timers;
+
+namespace Twenty2_V1._2
+{
+    class playMusic
+    {
+        public int TimeAllowed { get; set; }
+        public int BreakTime { get; set; }
+
+        public int BreakMultiplyFactor { get; set; }
+        public int MultiplyFactor { get; set; }
+        public int ButtonState { get; set; }
+        public int BreakButtonState { get; set; }
+        //public int BreakInterval { get; set; }
+
+        public bool on { get; set; }
+        public bool elapsedBool;
+        public Uri file { get; set; }
+        public string disFile { get; set; }
+
+        public System.Timers.Timer aTimer;
+
+        public MediaPlayer media = new MediaPlayer();
+        OpenFileDialog dialog = new OpenFileDialog();
+
+        #region Methods and Functions
+
+        #region Timer
+
+        public void elapsed()
+        {
+            aTimer.Stop();
+
+            int BreakInterval = BreakTime * BreakMultiplyFactor;
+
+            elapsedBool = true;
+            //media.Open(file);
+            media.Play();
+
+            Task.Delay(BreakInterval).Wait();
+
+            media.Stop();
+            media.Close();
+
+            aTimer.Start();
+        }
+
+        public void setTimer(int TimeAllowed)
+        {
+            int Interval = TimeAllowed * MultiplyFactor;
+
+            if (aTimer != null)
+                aTimer.Interval = Interval;
+            else
+                aTimer = new System.Timers.Timer(Interval);
+
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        public void onTimer()
+        {
+            if (on == true)
+            {
+                //aTimer.Elapsed += OnTimedEvent;
+                aTimer.Enabled = true;
+            }
+            else
+            {
+                aTimer.Enabled = false;
+            }
+        }
+
+        public void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            if (on == true)
+            {
+                elapsed();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        #endregion
+
+        #region fileGet
+
+        public string fileFind()
+        {
+            int disFileCount = 0;
+
+            dialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+
+            dialog.ShowDialog();
+
+            file = new Uri(dialog.FileName);
+
+            string disFile = file.ToString();
+
+            for (int i = disFile.Length - 1; i >= 0; i--)
+            {
+                disFileCount++;
+
+                if (disFile[i].Equals('/'))
+                {
+
+
+                    break;
+                }
+            }
+            return disFile.Substring(disFile.Length - disFileCount + 1);
+        }
+
+        #endregion
+
+        #region Hour Minute Buttons
+
+        public void breakButtonSwitchSeconds()
+        {
+            BreakMultiplyFactor = 1000;
+        }
+        public void buttonSwitchMinutes()
+        {
+            BreakMultiplyFactor = 60000;
+        }
+
+        public void buttonSwitchHours()
+        {
+            BreakMultiplyFactor = 3600000;
+        }
+
+        #endregion
+
+        public void buttonSwitchSeconds()
+        {
+            MultiplyFactor = 1000;
+        }
+        public void breakButtonSwitchMinutes()
+        {
+            MultiplyFactor = 60000;
+        }
+
+        public void breakButtonSwitchHours()
+        {
+            MultiplyFactor = 3600000;
+        }
+
+        #endregion
+    }
+}
